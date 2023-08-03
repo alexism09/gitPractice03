08eb6f16222191f2a7c9a9bad01a2632f0d2a821
alexism09 <alexismejia1121@gmail.com>
2023-08-02 21:07:47 -0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("this is a new code");
+        System.out.println("hi");
+        System.out.println("Batch17");
+        System.out.println("today is Wednesday");
 
 
         }
